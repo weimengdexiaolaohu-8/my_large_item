397bc2fbd8e1826bb0d52cd635512c965f887397
gaoxiang <1040776166@qq.com>
2021-05-12 14:48:58 +0800

    完成了index.html开发

--- assets/js/API.js
+++ assets/js/API.js
@@ -1,4 +1,17 @@
 $.ajaxPrefilter(function(option) {
     option.url = "http://api-breakingnews-web.itheima.net" + option.url
     console.log(option.url);
+    //统一为有权限的接口，设置请求头
+    if (option.url.indexOf("/my/") !== -1) {
+        option.headers = {
+            Authorization: localStorage.getItem("token") || ""
+        }
+    }
+    //统一配置无身份认证登陆
+    option.complete = function(res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            localStorage.removeItem("token")
+            location.href = "login.html"
+        }
+    }
 })
